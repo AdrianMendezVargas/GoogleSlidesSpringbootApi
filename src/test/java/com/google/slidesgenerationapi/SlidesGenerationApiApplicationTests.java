e23c18aee0327837403e0f03ece3e396f3d6bc1c
Adrian Alcides Mendez Vargas <adrian_mendez@ucne.edu.do>
2023-11-16 19:38:51 -0400

    init

---
+++ src/test/java/com/google/slidesgenerationapi/SlidesGenerationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.google.slidesgenerationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlidesGenerationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
